7f5dc55ffa2cd4775540ff0192098d5ec541a64e
Shahabr99 <sean.rohani9@gmail.com>
2023-03-19 22:56:03 -0700

    creating the word finder class

--- wordfinder.py
+++ wordfinder.py
@@ -1,5 +1,16 @@
 """Word Finder: finds random words from a dictionary."""
-
+import random
 
 class WordFinder:
-    ...
+    def __init__(self, path):
+        self.path = path
+
+    def random(self):
+        file = open(self.path, 'r')
+
+        word = file.read().split()
+       
+        random_word = random.choice(word)
+        print(random_word)
+
+        file.close()
